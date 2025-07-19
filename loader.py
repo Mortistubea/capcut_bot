75489b2e362e79451de6f1c075758fde3ffab4d4
Abdurahmon Muminov <155967262+Mortistubea@users.noreply.github.com>
2025-07-19 09:54:27 +0500

    Update loader.py

--- loader.py
+++ loader.py
@@ -2,8 +2,8 @@ from aiogram import Bot, Dispatcher, types
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 
 from data import config
-PROXY_URL = "156.228.95.10:3129"
 
-bot = Bot(token=config.BOT_TOKEN, parse_mode=types.ParseMode.HTML,proxy=PROXY_URL)
+
+bot = Bot(token=config.BOT_TOKEN, parse_mode=types.ParseMode.HTML)
 storage = MemoryStorage()
 dp = Dispatcher(bot, storage=storage)
