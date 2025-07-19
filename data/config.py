7bebfd8b8d493fd81c20442b5f85416d550c8d31
Abdurahmon Muminov <155967262+Mortistubea@users.noreply.github.com>
2025-07-19 08:27:40 +0500

    Update config.py

--- data/config.py
+++ data/config.py
@@ -3,7 +3,7 @@ from environs import Env
 # environs kutubxonasidan foydalanish
 env = Env()
 env.read_env()
-CHANNEL_ID = -1002169889277
+CHANNEL_ID = -1001622115740
 
 BOT_TOKEN = env.str("BOT_TOKEN")  # Bot token
 ADMINS = [int(x) for x in env.list("ADMINS")]  # Adminlar ro'yxati
